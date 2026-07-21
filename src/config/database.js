c85444868a2e3e4d25745d8646bdb8f5725657bf
Jarlin2419 <155831920+Jarlin2419@users.noreply.github.com>
2026-07-21 14:05:08 -0500

    Add connection listener for PostgreSQL pool1
    Added connection event listener to PostgreSQL pool.

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,14 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-
 const { Pool } = require('pg');
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
+
+module.exports = pool;
 
 module.exports = pool;
 
